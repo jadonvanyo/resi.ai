fe1e12dd806fd7c73cd8a266ee04f71ec32d8893
jayman779 <jadon.vanyo@gmail.com>
2024-03-06 23:27:13 -0500

    changed database to url for postgres

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import datetime
 from flask import Flask, jsonify, redirect, render_template, render_template_string, request, session
 from flask_session import Session
 import markdown
+import os
 import re
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -19,8 +20,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///resi.db")
+# CS50 library to connect with PostgreSQL database instead of SQLite database
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
 
 
 @app.after_request
